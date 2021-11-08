df5e8567a570644dc27470b6f0e20e9be30d477e
Raul Lendines <raullendines@gmail.com>
2021-11-08 11:42:23 +0100

    cambios header

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,39 +1,30 @@
-   
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body>
+
 <!--navbar-->
-<header class="header" id="inicio">
-        <span class="logo">
-            <img src="./media/Logo.png" href="https://www.geeksforgeeks.org/how-to-switch-the-language-of-the-page-using-javascript/" class="imgLogo">
-        </span>
-        <span class="menu">
-          <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div class="navbar-nav ms-auto marginNavBar">
-                <a class="nav-item nav-link" href="#">Administració</a>
-                <a class="nav-item nav-link" href="#">Jocs</a>
-                <a class="nav-item nav-link" href="#">Contacta</a>
-                <a class="nav-item nav-link" href="#">
-                 
-                </a>
-              </div>
-            </div>
-          </nav>
-        </span>
-    </header>
-
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-</html>
-
-
-
+    <nav class="navbar sticky-top navbar-expand-sm navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand text-black align-self-center" href="./index.php">
+            <img src="./media/logo_cep_simple.png" alt="Inicio" width="25" height="25" class="d-inline-block align-text-top">
+            Politecnics Barcelona
+          </a>
+          <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="justify-content-end nav collapse navbar-collapse">
+          <ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="">Disabled</a>
+            </li>
+          </ul>
+          </div>
+          
+        </div>
+    </nav> 
