0bc8892861e7ae7d34da3adba3375248ed586099
PerezSJ <sperezj2021@cepnet.net>
2021-11-08 20:05:05 +0100

    login page done. Pending HREF

--- php_views/login.php
+++ php_views/login.php
@@ -6,32 +6,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <!-- LINKS -->
-    <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.min.css">
-    <script src="bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
-    <!-- LINKS -->
+    <!-- BOOTSTRAP LINKS -->
+    <link rel="stylesheet" href="../bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <script src="../bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+    <!-- BOOTSTRAP LINKS -->
 </head>
 
 <body class="bg-dark">
-    <div class="container">
-        <div class="card card-login mx-auto mt150">
-            <div class="card-header">Login</div>
-            <div class="card-body">
-                <form method=post>
-                    <div class="form-group">
-                        <div class="form-label-group">
-                            <input type="username" id="username" class="form-control" placeholder="E-mail" required="required" autofocus>
-                            <label for="username">Usuario</label>
+    <div class=" container w-25">
+        <div class="col mt-5">
+            <div class="card">
+                <div class="card-header" style="text-align: center;">Login</div><br>
+                <div class="card-body">
+                    <form method=post>
+                        <div class="row">
+                            <label for="username" class="col-sm-4 col-form-label">Usuario</label>
+                            <div class="col-sm">
+                                <input type="username" id="username" class="form-control" placeholder="E-mail" required="required" autofocus>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="form-label-group">
-                            <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required="required">
-                            <label for="inputPassword">Contraseña</label>
+                        <div class="row pt-2">
+                            <label for="inputPassword" class="col-sm-4 col-form-label">Contraseña</label>
+                            <div class="col-sm">
+                                <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required="required">
+                            </div>
                         </div>
-                    </div>
-                    <input type="submit" class="btn btn-primary btn-block">
-                </form>
+                        <div class="row mt-4 justify-content-center">
+                            <input type="submit" class="btn btn-outline-success" href="#" value="Entrar">
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
