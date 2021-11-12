ea2fd1d866e8fd32c1fbb2486b4e85c4ff67d12c
PerezSJ <sperezj2021@cepnet.net>
2021-11-12 13:29:41 +0100

    background

--- indexAboutUs.php
+++ indexAboutUs.php
@@ -1,89 +1,78 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <title>About Us</title>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/styleAboutUs.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
     <script src="https://kit.fontawesome.com/27f55a2a2f.js" crossorigin="anonymous"></script>
-    <title>ABOUT US</title>
 </head>
 
 <body>
-
-    <h1 class="titulo">TeamWork</h1>
-    <div class="container">
-        <div class="row row-cols-1 row-cols-md-4 g-5 mt-4">
-            <div class="col">
-                <div class="card">
-                    <div class="front">
-                        <img src="/media/borja.jpeg" alt="">
-                    </div>
-                    <div class="back">
-                        <div class="back-content middle">
-                            <h2>Borja García</h2>
-                            <p>AQUÍ VA UNA DESCRIPCIÓN</p>
+    <div class="bg-dark">
+        <h1 class="titulo">TeamWork</h1>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-4 g-5 mt-4">
+                <div class="col">
+                    <div class="card">
+                        <div class="front">
+                            <img src="media/borja.jpeg" alt="">
+                        </div>
+                        <div class="back">
+                            <div class="back-content middle">
+                                <h2>Borja García</h2>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="col">
-                <div class="card">
-                    <div class="front">
-                        <img src="/media/sergi.jpeg" alt="">
-                    </div>
-                    <div class="back">
-                        <div class="back-content middle">
-                            <h2>Sergi Perez</h2>
-                            <p>AQUÍ VA UNA DESCRIPCIÓN</p>
+                <div class="col">
+                    <div class="card">
+                        <div class="front">
+                            <img src="media/sergi.jpeg" alt="">
+                        </div>
+                        <div class="back">
+                            <div class="back-content middle">
+                                <h2>Sergi Perez</h2>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="col">
-                <div class="card">
-                    <div class="front">
-                        <img src="/media/raul.jpeg" alt="">
-                    </div>
-                    <div class="back">
-                        <div class="back-content middle">
-                            <h2>Raul Lendines</h2>
-                            <p>AQUÍ VA UNA DESCRIPCIÓN</p>
+                <div class="col">
+                    <div class="card">
+                        <div class="front">
+                            <img src="media/raul.jpeg" alt="">
+                        </div>
+                        <div class="back">
+                            <div class="back-content middle">
+                                <h2>Raul Lendines</h2>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="col">
-                <div class="card">
-                    <div class="front">
-                        <img src="/media/aitor.png" alt="">
-                    </div>
-                    <div class="back">
-                        <div class="back-content middle">
-                            <h2>Aitor Carro</h2>
-                            <p>AQUÍ VA UNA DESCRIPCIÓN</p>
+                <div class="col">
+                    <div class="card">
+                        <div class="front">
+                            <img src="media/aitor.png" alt="">
+                        </div>
+                        <div class="back">
+                            <div class="back-content middle">
+                                <h2>Aitor Carro</h2>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-
 </body>
-
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
-    integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-    integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
-    crossorigin="anonymous"></script>
 
 </html>
