76713f5efdd609de1d6df8f3c10cdbf7741e1e2c
Raul Lendines <raullendines@gmail.com>
2021-11-12 11:23:02 +0100

    Cambio de titulo

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   <!-- Bootstrap css -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-  <title>Landing page</title>
+  <title>Home - Centre d'Estudis Politecncis</title>
 </head>
 
 <body>
