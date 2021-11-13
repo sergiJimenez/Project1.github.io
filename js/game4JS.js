d05bb0d26dc3a29835cb19e46419aaaf0e916bf8
PerezSJ <sperezj2021@cepnet.net>
2021-11-13 13:45:57 +0100

    fixing bugs

--- js/game4JS.js
+++ js/game4JS.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   const gurmann = document.createElement("div");
+  let isGameOver = false;
+  let speed = 3; //TO SETUP
+  let platformCount = 5;
+  let platforms = [];
+  let score = 0;
   let gurmannLeftSpace = 50;
   let startPoint = 150;
   let gurmannBottomSpace = startPoint;
-  let isGameOver = false;
-  let platformCount = 5;
-  let platforms = [];
+  const gravity = 0.9; //TO SETUP
   let upTimerId;
   let downTimerId;
   let isJumping = true;
@@ -14,19 +17,16 @@ document.addEventListener("DOMContentLoaded", () => {
   let isGoingRight = false;
   let leftTimerId;
   let rightTimerId;
-  let score = 0;
-
-  function createGurmann() {
-    grid.appendChild(gurmann);
-    gurmann.classList.add("gurmann");
-    gurmannLeftSpace = platforms[0].left;
-    gurmann.style.left = gurmannLeftSpace + "px";
-    gurmann.style.bottom = gurmannBottomSpace + "px";
-  }
+  //AIR
+  //AIR
+  //AIR
+  //AIR
+  //AIR
+  //Platforms
   class Platform {
     constructor(newPlatBottom) {
-      this.bottom = newPlatBottom;
       this.left = Math.random() * 315;
+      this.bottom = newPlatBottom;
       this.visual = document.createElement("div");
 
       const visual = this.visual;
@@ -43,13 +43,13 @@ document.addEventListener("DOMContentLoaded", () => {
       let newPlatBottom = 100 + i * platGap;
       let newPlatform = new Platform(newPlatBottom);
       platforms.push(newPlatform);
-      console.log(platforms);
+      console.log(platforms); //Only to check in the console if it's doing well
     }
   }
 
   function movePlatforms() {
     if (gurmannBottomSpace > 200) {
-      platforms.forEach((platform) => {
+      platforms.forEach(platform => {
         platform.bottom -= 4;
         let visual = platform.visual;
         visual.style.bottom = platform.bottom + "px";
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
           let firstPlatform = platforms[0].visual;
           firstPlatform.classList.remove("platform");
           platforms.shift();
-          console.log(platforms);
+          console.log(platforms); //Only to check in the console if it's doing well
           score++;
           let newPlatform = new Platform(600);
           platforms.push(newPlatform);
@@ -66,65 +66,63 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     }
   }
-
-  function jump() {
-    clearInterval(downTimerId);
-    isJumping = true;
-    upTimerId = setInterval(function () {
-      gurmannBottomSpace += 20;
-      gurmann.style.bottom = gurmannBottomSpace + "px";
-      if (gurmannBottomSpace > startPoint + 200) {
-        fall();
-      }
-    }, 30);
+  //Platforms
+  //AIR
+  //AIR
+  //AIR
+  //AIR
+  //AIR
+  //Gurmann, the character and his functions
+  function createGurmann() {
+    grid.appendChild(gurmann);
+    gurmann.classList.add("gurmann");
+    gurmannLeftSpace = platforms[0].left;
+    gurmann.style.left = gurmannLeftSpace + "px";
+    gurmann.style.bottom = gurmannBottomSpace + "px";
   }
 
   function fall() {
-    clearInterval(upTimerId);
     isJumping = false;
+    clearInterval(upTimerId);
     downTimerId = setInterval(function () {
       gurmannBottomSpace -= 5;
       gurmann.style.bottom = gurmannBottomSpace + "px";
       if (gurmannBottomSpace <= 0) {
         GameOver();
       }
-      platforms.forEach((platform) => {
+      platforms.forEach(platform => {
         if (
-          gurmannBottomSpace >= platform.bottom &&
-          gurmannBottomSpace <= platform.bottom + 15 &&
-          gurmannLeftSpace + 60 >= platform.left &&
-          gurmannLeftSpace <= platform.left + 85 &&
+          (gurmannBottomSpace >= platform.bottom) &&
+          (gurmannBottomSpace <= (platform.bottom + 15)) &&
+          ((gurmannLeftSpace + 60) >= platform.left) &&
+          (gurmannLeftSpace <= (platform.left + 85)) &&
           !isJumping
         ) {
-          console.log("Landed");
+          console.log("Landed"); //Only to check in the console if it's doing well
           startPoint = gurmannBottomSpace;
           jump();
+          console.log("start", startPoint); //Only to check in the console if it's doing well
+          isJumping = true;
         }
       });
-    }, 30);
+    }, 20);
   }
 
-  function GameOver() {
-    console.log("Game Over!");
-    isGameOver = true;
-    while (grid.firstChild) {
-      grid.removeChild(grid.firstChild);
-    }
-    grid.innerHTML = score;
-    clearInterval(upTimerId);
+  function jump() {
     clearInterval(downTimerId);
-    clearInterval(leftTimerId);
-    clearInterval(rightTimerId);
-  }
-
-  function control(e) {
-    if (e.key === "ArrowLeft") {
-      moveLeft();
-    } else if (e.key === "ArrowRight") {
-      moveRight();
-    } else if (e.key === "ArrowUp") {
-      moveStraight();
-    }
+    isJumping = true;
+    upTimerId = setInterval(function () {
+      console.log(startPoint); //Only to check in the console if it's doing well
+      console.log("1", gurmannBottomSpace); //Only to check in the console if it's doing well
+      gurmannBottomSpace += 20;
+      gurmann.style.bottom = gurmannBottomSpace + "px";
+      console.log("2", gurmannBottomSpace); //Only to check in the console if it's doing well
+      console.log("s", startPoint); //Only to check in the console if it's doing well
+      if (gurmannBottomSpace > (startPoint + 200)) {
+        fall();
+        isJumping = false;
+      }
+    }, 30);
   }
 
   function moveLeft() {
@@ -135,6 +133,7 @@ document.addEventListener("DOMContentLoaded", () => {
     isGoingLeft = true;
     leftTimerId = setInterval(function () {
       if (gurmannLeftSpace >= 0) {
+        console.log("LEFT!"); //Only to check in the console if it's doing well
         gurmannLeftSpace -= 5;
         gurmann.style.left = gurmannLeftSpace + "px";
       } else moveRight();
@@ -148,7 +147,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     isGoingRight = true;
     rightTimerId = setInterval(function () {
+      //POSIBLE HAD TO CHANGE THE gurmannLeftSpace TO FIT .PNG
       if (gurmannLeftSpace <= 340) {
+        console.log("RIGHT!"); //Only to check in the console if it's doing well
         gurmannLeftSpace += 5;
         gurmann.style.left = gurmannLeftSpace + "px";
       } else moveLeft();
@@ -156,10 +157,41 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function moveStraight() {
-    isGoingRight = false;
     isGoingLeft = false;
-    clearInterval(rightTimerId);
+    isGoingRight = false;
     clearInterval(leftTimerId);
+    clearInterval(rightTimerId);
+  }
+
+  function control(e) {
+    gurmann.style.bottom = gurmannBottomSpace + "px";
+    if (e.key === "ArrowLeft") {
+      moveLeft();
+    } else if (e.key === "ArrowRight") {
+      moveRight();
+    } else if (e.key === "ArrowUp") {
+      moveStraight();
+    }
+  }
+  //Gurmann, the character and his functions
+  //AIR
+  //AIR
+  //AIR
+  //AIR
+  //AIR
+  //Game Functions
+  function GameOver() {
+    isGameOver = true;
+    console.log("Game Over!"); //Only to check in the console if it's doing well
+    while (grid.firstChild) {
+      console.log("Again!"); //Only to check in the console if it's doing well
+      grid.removeChild(grid.firstChild);
+    }
+    grid.innerHTML = score;
+    clearInterval(upTimerId);
+    clearInterval(downTimerId);
+    clearInterval(leftTimerId);
+    clearInterval(rightTimerId);
   }
 
   function start() {
@@ -167,10 +199,9 @@ document.addEventListener("DOMContentLoaded", () => {
       createPlatforms();
       createGurmann();
       setInterval(movePlatforms, 30);
-      jump();
+      jump(startPoint);
       document.addEventListener("keyup", control);
     }
   }
-  //attach to button:
   start();
 });
