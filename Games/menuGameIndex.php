ef6ead2384f4dd20a492261308eb431321c59483
PerezSJ <sperezj2021@cepnet.net>
2021-11-25 10:28:08 +0100

    reorganize

--- Games/menuGameIndex.php
+++ Games/menuGameIndex.php
@@ -6,7 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu Game</title>
 </head>
-<body>
-  
-</body>
+    <body>
+        <div class = "grid">
+            <div class = "BCKGRND">
+                <button type="button" class=""></button>
+            </div>
+        </div>
+    </body>
 </html>
