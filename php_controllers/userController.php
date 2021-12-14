d9a1a1b3d74e4189466c320a3c33383919015242
Raul Lendines <raullendines@gmail.com>
2021-12-14 16:55:35 +0100

    s

---
+++ php_controllers/userController.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once("../php_library/database.php");
+
+if (isset($_POST['insert'])) {
+    if ($_POST['inputPassword'] == $_POST['inputConfirmPassword']) {
+        insertUser($_POST['inputFirstName'], $_POST['inputEmail'], $_POST['dropdownMenuButton'], $_POST['inputPassword']);
+
+        header("Location: ../menuGames/games/game3Raul/rankingGameRaul/ranking.php");
+        exit();
+    }
+    else{
+        echo "las contraseñas no son iguales";
+        header("Location: google.es");
+        exit();
+    }
+}
+else{
+    header("Location: google.es");
+    exit();
+}
+?>
