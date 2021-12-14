aed32687e047a3e489f2b03320b0616e7f2cfc07
Raul Lendines <raullendines@gmail.com>
2021-12-14 16:40:03 +0100

    m

--- form/registerUsers.php
+++ form/registerUsers.php
@@ -17,7 +17,7 @@ if (isset($_SESSION["correcto"])) {
 
 require_once('./../php_library/database.php');
 $ciclos = selectCiclos();
-echo $ciclos;
+var_dump($ciclos);
 ?>
 
 <!DOCTYPE html>
@@ -61,23 +61,10 @@ echo $ciclos;
                                                 <div class="col-sm-10">
                                                     <select class="form-control" name="ciclo" id="cicloUsuario" required>
                                                         <option value="">Formative Grade</option>
-                                                        <!-- Comerç i Màrqueting -->
-                                                        <option <?php if(isset($usuario)){if ($usuario['id_Ciclo'] == "1") {echo "selected";}} ?> value="1">Grau mitjà activitats comercials</option>
-                                                        <option <?php if(isset($usuario)){if ($usuario['id_Ciclo'] == "2") {echo "selected";}} ?> value="2">Grau superior màrqueting</option>
-                                                        <option <?php if(isset($usuario)){if ($usuario['id_Ciclo'] == "3") {echo "selected";}} ?> value="3">Grau superior gestió de vendes i espais comercials</option>
-                                                        <option <?php if(isset($usuario)){if ($usuario['id_Ciclo'] == "4") {echo "selected";}} ?> value="4">Grau superior comerç internacional</option>
-                                                        <!-- Hoteleria i Turisme -->
-                                                        <option <?php if(isset($usuario)){if ($usuario['id_Ciclo'] == "5") {echo "selected";}} ?> value="5">Grau superior agències de viatges i gestió d'esdeveniments</option>
-                                                        <!-- Informàtica i comunicacions -->
-                                                        <option <?php if(isset($usuario)){if ($usuario['id_Ciclo'] == "6") {echo "selected";}} ?> value="6">Grau mitjà sistemes microinformàtics i xarxes</option>
-                                                        <option <?php if(isset($usuario)){if ($usuario['id_Ciclo'] == "7") {echo "selected";}} ?> value="7">Grau superior desenvolupaments aplicacions multiplataforma</option>
-                                                        <option <?php if(isset($usuario)){if ($usuario['id_Ciclo'] == "8") {echo "selected";}} ?> value="8">Grau superior desenvolupaments aplicacions web</option>
-                                                        <!-- Administració i gestió -->
-                                                        <option <?php if(isset($usuario)){if ($usuario['id_Ciclo'] == "9") {echo "selected";}} ?> value="9">Grau mitjà gestió administrativa</option>
-                                                        <option <?php if(isset($usuario)){if ($usuario['id_Ciclo'] == "10") {echo "selected";}} ?> value="10">Grau superior assistència a la direcció</option>
 
-                                                        <!--FOREACH-->
-                                                        <option <?php if(isset($usuario)){if ($usuario['id_Ciclo'] == "11") {echo "selected";}} ?> value="11">Grau superior administració i finances</option>
+                                                        <?php foreach ($ciclos as $ciclo) { ?>
+                                                            <option <?php if(isset($usuario)){if ($usuario['id_Ciclo'] == $ciclo['id']) {echo "selected";}} ?> value= <?php $ciclo['id'] ?> > <?php $ciclo['Nombre_Ciclos'] ?> </option>
+                                                        <?php } ?>
                                                     </select>
                                                 </div>
                                             </div>
